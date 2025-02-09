ea5d15e072278ea7db5f50d9fbe5182f70a871a8
Madina <yermagambetovva@gmail.com>
2025-02-09 16:10:47 +0500

    lab4

---
+++ Lab4/Modules/mymodule.py
@@ -0,0 +1,18 @@
+def greeting(name):
+  print("Hello, " + name)
+
+
+person1 = {
+  "name": "John",
+  "age": 36,
+  "country": "Norway"
+}
+
+def greeting(name):
+  print("Hello, " + name)
+
+person1 = {
+  "name": "John",
+  "age": 36,
+  "country": "Norway"
+}
